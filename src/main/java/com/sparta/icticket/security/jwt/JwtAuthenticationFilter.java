17c81bc283ac6d54e70c21356633d10cd5c47412
jay1261 <dongjae73@naver.com>
2024-07-22 22:12:40 +0900

    [Add] Security Filter 추가 #9

---
+++ src/main/java/com/sparta/icticket/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,92 @@
+package com.sparta.icticket.security.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.icticket.common.dto.ResponseMessageDto;
+import com.sparta.icticket.common.enums.ErrorType;
+import com.sparta.icticket.common.enums.SuccessStatus;
+import com.sparta.icticket.common.enums.UserRole;
+import com.sparta.icticket.common.enums.UserStatus;
+import com.sparta.icticket.common.exception.CustomException;
+import com.sparta.icticket.common.exception.ExceptionDto;
+import com.sparta.icticket.security.UserDetailsImpl;
+import com.sparta.icticket.user.User;
+import com.sparta.icticket.user.UserRepository;
+import com.sparta.icticket.user.dto.LoginRequestDto;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+
+@Slf4j(topic = "로그인 및 JWT 생성")
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil, UserRepository userRepository) {
+        this.jwtUtil = jwtUtil;
+        this.userRepository = userRepository;
+        setFilterProcessesUrl("/users/login");
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            LoginRequestDto requestDto = new ObjectMapper().readValue(request.getInputStream(), LoginRequestDto.class);
+
+            return getAuthenticationManager().authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            requestDto.getEmail(),
+                            requestDto.getPassword(),
+                            null
+                    )
+            );
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    // 로그인 성공시 처리
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException {
+        User user = ((UserDetailsImpl) authResult.getPrincipal()).getUser();
+        if(user.getUserStatus().equals(UserStatus.DEACTIVATE)){
+            throw new CustomException(ErrorType.NOT_FOUND_USER);
+        }
+
+        String username = ((UserDetailsImpl) authResult.getPrincipal()).getUsername();
+        UserRole role = ((UserDetailsImpl) authResult.getPrincipal()).getUser().getUserRole();
+
+        String accessToken = jwtUtil.createAccessToken(username, role);
+        String refreshToken = jwtUtil.createRefreshToken(username, role);
+
+        user.saveRefreshToken(refreshToken.substring(7));
+        userRepository.save(user);
+
+        response.addHeader(JwtUtil.AUTH_ACCESS_HEADER, accessToken);
+        response.addHeader(JwtUtil.AUTH_REFRESH_HEADER, refreshToken);
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(new ObjectMapper().writeValueAsString(ResponseEntity.ok().body(new ResponseMessageDto(SuccessStatus.USER_LOGIN_SUCCESS))));
+        response.getWriter().flush();
+    }
+
+    // 로그인 실패시 처리
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException {
+        ErrorType errorType = ErrorType.NOT_FOUND_AUTHENTICATION_INFO;
+        response.setStatus(errorType.getHttpStatus().value());
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(new ObjectMapper().writeValueAsString(ResponseEntity.status(errorType.getHttpStatus()).body(new ExceptionDto(errorType))));
+        response.getWriter().flush();
+    }
+}
