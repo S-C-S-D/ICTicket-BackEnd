8e4970c75d7faf0b239d6a9a34a111e14aaceea2
한근형 <165651118+hangeunhyeong@users.noreply.github.com>
2024-07-22 12:08:14 +0900

    [Add] Performance Entity 추가 #1

---
+++ src/main/java/com/sparta/icticket/performance/Performance.java
@@ -0,0 +1,51 @@
+package com.sparta.icticket.performance;
+
+import com.sparta.icticket.common.Timestamped;
+import com.sparta.icticket.common.enums.AgeGroup;
+import com.sparta.icticket.common.enums.Genre;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+public class Performance extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "venue_id",nullable = false)
+    private Venue venue;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Genre genre;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AgeGroup ageGroup;
+
+    @Column(nullable = false)
+    private String runTime;
+
+    @Column(nullable = false)
+    private LocalDateTime openAt;
+
+    @Column(nullable = false)
+    private LocalDate startAt;
+
+    @Column(nullable = false)
+    private LocalDate endAt;
+
+    private String image;
+
+    private Long viewCount;
+
+}
