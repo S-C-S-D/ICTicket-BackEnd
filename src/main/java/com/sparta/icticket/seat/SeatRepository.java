59f517c423c699d49ed187ce866c6ea9c138eb72
Lee0zoo <leeyj4515@naver.com>
2024-07-23 17:53:17 +0900

    [Fix] 세션별 잔여 좌석 조회 쿼리문 작성 #23

--- src/main/java/com/sparta/icticket/seat/SeatRepository.java
+++ src/main/java/com/sparta/icticket/seat/SeatRepository.java
@@ -2,9 +2,11 @@ package com.sparta.icticket.seat;
 
 import com.sparta.icticket.session.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SeatRepository extends JpaRepository<Seat, Long> {
     Integer countBySession(Session findSession);
 
+    @Query("SELECT count(*) from Seat where session = :findSession and isReserved = :b")
     Integer countBySessionAndReserved(Session findSession, boolean b);
 }
