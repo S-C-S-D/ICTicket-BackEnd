e4ef4ad33cba0b0306aa4711d8a5e54c39b8c1f3
Lee0zoo <leeyj4515@naver.com>
2024-07-23 14:20:50 +0900

    [Chore] Repository 추가 #10

---
+++ src/main/java/com/sparta/icticket/banner/BannerRepository.java
@@ -0,0 +1,6 @@
+package com.sparta.icticket.banner;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BannerRepository extends JpaRepository<Banner, Long> {
+}
---
+++ src/main/java/com/sparta/icticket/seat/SeatRepository.java
@@ -0,0 +1,6 @@
+package com.sparta.icticket.seat;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SeatRepository extends JpaRepository<Seat, Long> {
+}
