cc575462932c954a602bc3f23bd0ef6ef305de24
이도 <skydoya191539@naver.com>
2024-07-23 10:07:23 +0900

    init

---
+++ src/main/java/com/sparta/icticket/admin/venue/VenueAdminController.java
@@ -0,0 +1,10 @@
+package com.sparta.icticket.admin.venue;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/venues")
+public class VenueAdminController {
+
+
+}
---
+++ src/main/java/com/sparta/icticket/admin/venue/VenueAdminService.java
@@ -0,0 +1,4 @@
+package com.sparta.icticket.admin.venue;
+
+public class VenueAdminService {
+}
