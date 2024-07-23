718671412e29bf6aef98011709b9a8caf18a4a6a
한근형 <165651118+hangeunhyeong@users.noreply.github.com>
2024-07-23 10:40:51 +0900

    [Chore] User Entity에 unique 추가 #1

--- src/main/java/com/sparta/icticket/user/User.java
+++ src/main/java/com/sparta/icticket/user/User.java
@@ -18,7 +18,7 @@ public class User extends Timestamped {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false,unique = true)
     private String email;
 
     @Column(nullable = false)
@@ -27,7 +27,7 @@ public class User extends Timestamped {
     @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false)
+    @Column(nullable = false,unique = true)
     private String nickname;
 
     @Column(nullable = false)
