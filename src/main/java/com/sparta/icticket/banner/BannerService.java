2474a13d710d7be5f9fa1b3ac17cff4e04251e88
jay1261 <dongjae73@naver.com>
2024-07-25 11:54:03 +0900

    [Feat] 배너 타입별 배너 조회 #36

---
+++ src/main/java/com/sparta/icticket/banner/BannerService.java
@@ -0,0 +1,26 @@
+package com.sparta.icticket.banner;
+
+import com.sparta.icticket.banner.dto.BannerResponseDto;
+import com.sparta.icticket.common.enums.BannerType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BannerService {
+
+    private final BannerRepository bannerRepository;
+
+    /**
+     * 배너 타입별 배너 조회(10개)
+     * @param bannerType
+     * @return
+     */
+    public List<BannerResponseDto> getBannersByType(BannerType bannerType) {
+        List<Banner> bannerList = bannerRepository.findTop10ByBannerTypeOrderByPosition(bannerType);
+
+        return bannerList.stream().map(BannerResponseDto::new).toList();
+    }
+}
