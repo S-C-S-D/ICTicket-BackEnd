afb0d96628110540f2def7e604055cc34c143859
jay1261 <dongjae73@naver.com>
2024-07-26 19:21:24 +0900

    [Chore] HttpMessageNotReadableException 예외처리 추가 및 에러타입 정리 #58

--- src/main/java/com/sparta/icticket/common/enums/ErrorType.java
+++ src/main/java/com/sparta/icticket/common/enums/ErrorType.java
@@ -12,7 +12,6 @@ public enum ErrorType {
     ALREADY_EXISTS_EMAIL(HttpStatus.LOCKED, "이미 존재하는 이메일입니다."),
     ALREADY_EXISTS_NICKNAME(HttpStatus.LOCKED, "이미 존재하는 닉네임입니다."),
     NOT_FOUND_USER(HttpStatus.LOCKED, "존재하지 않는 회원입니다."),
-    NOT_FOUND_EMAIL(HttpStatus.LOCKED, "존재하지 않는 이메일입니다."),
     INVALID_PASSWORD(HttpStatus.LOCKED, "비밀번호가 일치하지 않습니다."),
     DEACTIVATE_USER(HttpStatus.LOCKED, "탈퇴한 회원입니다."),
     NOT_AVAILABLE_PERMISSION(HttpStatus.LOCKED, "권한이 없습니다."),
@@ -21,8 +20,6 @@ public enum ErrorType {
     END_AT_PASSED_START_AT(HttpStatus.LOCKED, "공연 마감일이 시작일보다 빠릅니다."),
     WRONG_DATE_FORMAT(HttpStatus.LOCKED, "잘못된 날짜 입니다."),
     NOT_FOUND_PERFORMANCE(HttpStatus.LOCKED, "존재하지 않는 공연입니다."),
-    NOT_FOUND_TODAY_OPEN_PERFORMANCES(HttpStatus.LOCKED, "오픈 예정인 공연이 없습니다."),
-    NOT_FOUND_GENRE(HttpStatus.LOCKED, "존재하지 않는 장르입니다."),
 
     //[LIKE]
     ALREADY_LIKED_PERFORMANCE(HttpStatus.LOCKED, "이미 관심공연으로 등록한 공연입니다."),
@@ -48,15 +45,11 @@ public enum ErrorType {
 
     //[AUTHENTICATION]
     REQUIRES_LOGIN(HttpStatus.LOCKED, "로그인이 필요한 서비스입니다."),
-    INVALID_ACCESS(HttpStatus.BAD_REQUEST, "잘못된 접근입니다."),
 
     //[JWT]
     INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 리프레시 토큰입니다. 다시 로그인 해주세요."),
     NOT_FOUND_AUTHENTICATION_INFO(HttpStatus.NOT_FOUND, "인증 정보를 찾을 수 없습니다."),
-    INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다. 다시 로그인 해주세요."),
-    LOGGED_OUT_TOKEN(HttpStatus.FORBIDDEN, "이미 로그아웃된 토큰입니다."),
     INVALID_JWT(HttpStatus.UNAUTHORIZED, "유효하지 않는 JWT 입니다."),
-    EXPIRED_JWT(HttpStatus.FORBIDDEN, "만료된 JWT 입니다."),
 
     // [DISCOUNT]
     NOT_FOUND_SALES(HttpStatus.LOCKED, "존재하지 않는 할인 정보입니다."),
@@ -66,8 +59,6 @@ public enum ErrorType {
     NOT_YOUR_ORDER(HttpStatus.LOCKED,"예매 취소 권한이 없습니다."),
     NOT_FOUND_ORDER(HttpStatus.LOCKED,"예매 내역이 존재하지 않습니다"),
     ALREADY_CANCELED_ORDER(HttpStatus.LOCKED,"이전에 예매 취소한 이력이 있습니다."),
-
-
 
     // [BANNER]
     NOT_FOUND_BANNER(HttpStatus.NOT_FOUND, "존재하지 않는 배너입니다."),
