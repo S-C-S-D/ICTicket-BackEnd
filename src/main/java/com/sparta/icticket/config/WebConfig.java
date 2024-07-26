b3d0067161ca1ea36524eca1b0d9bbec0281ba03
Yeahyun Kim <insidesy4@gmail.com>
2024-07-26 14:22:32 +0900

    [Feat] localhost:3000, 5500, 8080 요청 허가 처리 #53

--- src/main/java/com/sparta/icticket/config/WebConfig.java
+++ src/main/java/com/sparta/icticket/config/WebConfig.java
@@ -1,4 +1,20 @@
 package com.sparta.icticket.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:5500")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
