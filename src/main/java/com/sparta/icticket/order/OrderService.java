279d97939101d28066dc64d99571a4427266f90e
Yeahyun Kim <insidesy4@gmail.com>
2024-08-06 11:24:01 +0900

    [Test] 코드리뷰를 위한 주석 추가

--- src/main/java/com/sparta/icticket/order/OrderService.java
+++ src/main/java/com/sparta/icticket/order/OrderService.java
@@ -43,14 +43,21 @@ public class OrderService {
      */
     @Transactional
     public OrderCreateResponseDto createOrder(Long sessionId, OrderCreateRequestDto requestDto, User loginUser) {
+        //0. 결제하기 버튼을 눌렀을 때, 실행되는 로직입니다.
+        //0. 현재 유저는 좌석 선택 완료 버튼을 누르고, 결제하기 페이지에 있는 상황입니다.
+        //   버튼에는 2가지가 있고, 좌석 선택 완료 버튼 -> 결제 완료 버튼 순으로 진행이 됩니다.
+        //    - 좌석 선택 완료 버튼
+        //    - 결제 완료 버튼
 
+        // 1.결제하기 버튼을 누르기 전에 좌석 선택 완료 버튼을 눌렀을 때, 버튼 누른 시간을 프론트에 저장했다가, 결제하기 버튼을 눌렀을 때 백앤드에 보내줍니다.
         LocalDateTime reservedAt = requestDto.getModifiedAt();
 
+        // 2.프론트에서 보내준 좌석 선택완료 버튼을 클릭한 시간이 현재 시간과 10분 이상 차이가 나면, 결제가 되지 않고 예외처리로 넘어갑니다.
         if(reservedAt.isBefore(LocalDateTime.now().minusMinutes(10))) {
             throw new CustomException(ErrorType.TIME_OUT);
         }
 
-        // 세션 찾기
+        // 3. 아래는 결제에 필요한 로직이 수행됩니다. 무시하셔도 됩니다.
         Session findSession = findSessionById(sessionId);
 
         List<String> findSeatNumberList = new ArrayList<>();
