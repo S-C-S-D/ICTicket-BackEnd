021cb5d0d12ad010689fb8bd2afcd7fcf00b4d2d
Lee0zoo <leeyj4515@naver.com>
2024-08-08 17:22:50 +0900

    [Chore] 쿼리 dsl 수정 #92

--- src/main/java/com/sparta/icticket/seat/SeatRepositoryQueryImpl.java
+++ src/main/java/com/sparta/icticket/seat/SeatRepositoryQueryImpl.java
@@ -26,6 +26,7 @@ public class SeatRepositoryQueryImpl implements SeatRepositoryQuery {
         queryFactory
                 .update(seat)
                 .set(seat.seatStatus, SeatStatus.NOT_RESERVED)
+                .set(seat.userId, (Long) null)
                 .where(seat.seatStatus.eq(SeatStatus.PAYING).and(seat.seatSelectedAt.before(LocalDateTime.now().minusMinutes(10))))
                 .execute();
     }
