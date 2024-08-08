9aa0d40220436e8cd9bdb0e6597fd0536ad64fd0
Lee0zoo <leeyj4515@naver.com>
2024-08-08 10:29:53 +0900

    [Fix] Timestamped LocalDateTime을 Timestamp로 변경 #82

--- src/main/java/com/sparta/icticket/common/Timestamped.java
+++ src/main/java/com/sparta/icticket/common/Timestamped.java
@@ -5,7 +5,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
+import java.sql.Timestamp;
 
 @Getter
 @MappedSuperclass
@@ -15,10 +15,10 @@ public abstract class Timestamped {
     @CreatedDate
     @Column(updatable = false)
     @Temporal(TemporalType.TIMESTAMP)
-    private LocalDateTime createdAt;
+    private Timestamp createdAt;
 
     @LastModifiedDate
     @Column
     @Temporal(TemporalType.TIMESTAMP)
-    private LocalDateTime modifiedAt;
+    private Timestamp modifiedAt;
 }
