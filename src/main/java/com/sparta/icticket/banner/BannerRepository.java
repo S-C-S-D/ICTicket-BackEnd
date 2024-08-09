6ecc87396475757e18bdd80e27f68680dd0e77be
Lee0zoo <leeyj4515@naver.com>
2024-08-09 10:44:27 +0900

    [Refactor] banner startAt, endAt 추가 #100

--- src/main/java/com/sparta/icticket/banner/BannerRepository.java
+++ src/main/java/com/sparta/icticket/banner/BannerRepository.java
@@ -6,10 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface BannerRepository extends JpaRepository<Banner, Long> {
-
-    //주어진 배너 타입에 따라 위치를 기준으로 내림차순 정렬된 상위 10개의 배너를 반환합니다.
-    List<Banner> findTop10ByBannerTypeOrderByPositionDesc(BannerType bannerType);
+public interface BannerRepository extends JpaRepository<Banner, Long>, BannerRepositoryQuery {
 
     //주어진 위치에 해당하는 배너를 반환합니다.
     Optional<Banner> findByPosition(Integer position);
