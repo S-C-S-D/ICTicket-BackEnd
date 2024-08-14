ccf1a8dee4fa88ec5dff9500ec4eda96ca08cce7
Lee0zoo <leeyj4515@naver.com>
2024-08-14 11:38:16 +0900

    [Test] Seat Service 테스트 코드 수정 및 dto 수정 #106

--- src/test/java/com/sparta/icticket/seat/SeatServiceTest.java
+++ src/test/java/com/sparta/icticket/seat/SeatServiceTest.java
@@ -22,6 +22,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -78,18 +80,22 @@ class SeatServiceTest {
         List<Seat> list = new ArrayList<>();
         list.add(seat);
 
-        when(seat.getSeatGrade()).thenReturn(SeatGrade.A);
-        when(seat.getPrice()).thenReturn(10000);
         when(sessionRepository.findById(any(Long.class))).thenReturn(Optional.of(session));
         doNothing().when(session).checkPerformance(any(Long.class));
-
         when(seatRepository.findAllBySessionId(1L)).thenReturn(list);
+
+        when(seat.getId()).thenReturn(1L);
+        when(seat.getSeatGrade()).thenReturn(SeatGrade.A);
+        when(seat.getSeatStatus()).thenReturn(SeatStatus.PAYING);
+        when(seat.getPrice()).thenReturn(10000);
 
         // when
         List<SeatInfoResponseDto> seatList = seatService.getSeats(1L, 1L);
 
         // then
+        assertEquals(1L, seatList.get(0).getId());
         assertEquals(SeatGrade.A, seatList.get(0).getSeatGrade());
+        assertEquals(SeatStatus.PAYING, seatList.get(0).getSeatStatus());
         assertEquals(10000, seatList.get(0).getPrice());
     }
 
@@ -113,23 +119,40 @@ class SeatServiceTest {
         User user = new User();
 
         Performance performance = Mockito.mock(Performance.class);
+        when(session.getPerformance()).thenReturn(performance);
 
         Sales sales = Mockito.mock(Sales.class);
-        when(sales.getDiscountRate()).thenReturn(10);
 
         when(sessionRepository.findById(any(Long.class))).thenReturn(Optional.of(session));
         when(seatRepository.findSeatsByIdList(seatIds)).thenReturn(seatList);
         doNothing().when(seat).checkSession(any(Long.class));
         doNothing().when(seat).updateSeatStatusToPaying(user);
-        when(session.getPerformance()).thenReturn(performance);
         when(salesRepository.findByPerformance(performance)).thenReturn(Optional.of(sales));
+
+
+        when(performance.getTitle()).thenReturn("title");
+        when(performance.getImageUrl()).thenReturn("abcde");
+
+        when(session.getSessionDate()).thenReturn(LocalDate.now());
+        when(session.getSessionTime()).thenReturn(LocalTime.of(12, 30, 0));
+        when(session.getSessionName()).thenReturn("A회차");
+
+        when(sales.getDiscountRate()).thenReturn(10);
+
+        List<String> seatNumberList = new ArrayList<>();
+        seatNumberList.add("1");
 
         // when
         SeatReservedResponseDto responseDto = seatService.reserveSeat(1L, requestDto, user);
 
         // then
+        assertEquals("title", responseDto.getTitle());
+        assertEquals("abcde", responseDto.getImageUrl());
+        assertEquals(LocalDate.now(), responseDto.getSessionDate());
+        assertEquals(LocalTime.of(12, 30, 0), responseDto.getSessionTime());
+        assertEquals("A회차", responseDto.getSessionName());
+        assertEquals(seatNumberList, responseDto.getSeatNumberList());
+        assertEquals(9000, responseDto.getTotalPrice());
         assertEquals(10, responseDto.getDiscountRate());
-        assertEquals(10000, responseDto.getTotalPrice());
-        assertEquals("1", responseDto.getSeatNumberList().get(0));
     }
 }
